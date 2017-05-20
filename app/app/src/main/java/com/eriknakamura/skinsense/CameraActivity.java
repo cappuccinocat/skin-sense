5d22a7658af1fc0cf8ed637c6b8718753dd0e7a0
cappuccinocat <xinglydia@gmail.com>
2017-05-20 17:44:38 -0400

    now can take picture and save it to phone

--- app/app/src/main/java/com/eriknakamura/skinsense/CameraActivity.java
+++ app/app/src/main/java/com/eriknakamura/skinsense/CameraActivity.java
@@ -1,26 +1,33 @@
 package com.eriknakamura.skinsense;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.hardware.Camera;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.text.DateFormat;
+import java.util.Date;
+
 public class CameraActivity extends AppCompatActivity {
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
     ImageView imageView;
     Button submitInfo;
     Button takePicture;
+    Button test;
     CheckBox itchiness;
     CheckBox burning;
     CheckBox pain;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +54,16 @@ public class CameraActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 intent.putExtra("itchiness", itchiness.isChecked());
+                intent.putExtra("stringPath", currentPhotoPath);
                 startActivity(intent);
+            }
+        });
+
+        test = (Button) findViewById(R.id.button);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setThumbnail(currentPhotoPath);
             }
         });
     }
@@ -55,17 +71,40 @@ public class CameraActivity extends AppCompatActivity {
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.eriknakamura.skinsense",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-        }
+    private File createImageFile() throws Exception {
+        String timeStamp = DateFormat.getDateTimeInstance().format(new Date()).replaceAll(" ", "").replaceAll(":", "").replaceAll(",", "");
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+
+        currentPhotoPath = image.getAbsolutePath();
+        System.out.println(currentPhotoPath);
+        return image;
+    }
+
+    private void setThumbnail(String path){
+        Bitmap bmImg = BitmapFactory.decodeFile(path);
+        imageView.setImageBitmap(bmImg);
     }
 }
-
